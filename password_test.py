d74c3f5d1649835ffc926aab66d0718176cf3aa0
faith62 <phifeichemu@gmail.com>
2022-04-24 20:36:40 +0300

    test save password

--- password_test.py
+++ password_test.py
@@ -9,11 +9,11 @@ class TestPassword(unittest.TestCase):
     Args:
         unittest.TestCase: TestCase class that helps in creating test cases
     '''
-def setUp(self):
+def setUp(self): #allows us to define instructions that will be executed before each test method.
         '''
         Set up method to run before each test cases.
         '''
-        self.new_password = Password("Faith", "123456") # create password object
+        self.new_password = Password("Faith", "123456") # create new instance of password class, store in an instance variable in test class self.new_password
 
 
 def test_init(self):
@@ -23,7 +23,18 @@ def test_init(self):
 
     self.assertEqual(self.new_password.user_name,"Faith")
     self.assertEqual(self.new_password.password,"123456")
+
+def test_save_password(self):
+    '''
+    test_save_password test case to test if the password object is saved into
+        the password list
+    '''
+    self.new_password.save_password() # saving the new password
+    self.assertEqual(len(Password.password_list),1)
+
+
     
 
-if __name__ == '__main__':
-    unittest.main()
+if __name__ == '__main__': #confirming that anything inside the if block should run only if this is the file that is currently being run
+    unittest.main() #provides a command line interface that collects all the tests methods and executes them.
+
