5f51bbba3e3cf22a72c1d0cfc8200fe49d1afe11
faith62 <phifeichemu@gmail.com>
2022-04-24 20:39:14 +0300

    create a save_contact method that is called on a password object and appends it to our password-list list.

--- password.py
+++ password.py
@@ -3,10 +3,18 @@ class Password:
     Class that generates new instances of password
     """
     password_list =  [] #used to store our created password objects 
-    
+
     def __init__(self,user_name,password):
         self.user_name = user_name
         self.password = password
+
+    def save_password(self):
+
+        '''
+        save_password method saves password objects into password_list
+        '''
+
+        Password.password_list.append(self)
         
 
     
