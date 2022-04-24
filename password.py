5febb1518c805c6b572ac62ea4b4980d03b752e2
faith62 <phifeichemu@gmail.com>
2022-04-24 20:15:30 +0300

    create a class password, and instance variables

---
+++ password.py
@@ -0,0 +1,10 @@
+class Password:
+    """
+    Class that generates new instances of password
+    """
+    def __init__(self,user_name,password):
+        self.user_name = user_name
+        self.password = password
+        
+
+    
