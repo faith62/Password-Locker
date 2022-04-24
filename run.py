041f2970276d1319937f42f75f6618f425485f92
faith62 <phifeichemu@gmail.com>
2022-04-25 00:51:52 +0300

    create a password function

--- run.py
+++ run.py
@@ -2,3 +2,9 @@
 
 from password import Password
 
+def create_password(uname,pwd):
+    '''
+    Function to create a new password
+    '''
+    new_password = Password(uname,pwd)
+    return new_password
