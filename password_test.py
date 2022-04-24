991ed6a3f423f5c61ab9fb6b5c73579f18f309f0
faith62 <phifeichemu@gmail.com>
2022-04-24 20:28:22 +0300

    test if objects are being instantiated correctly.

--- password_test.py
+++ password_test.py
@@ -1,11 +1,29 @@
 import unittest # Importing the unittest module
-from password import Password # Importing the contact class
+from password import Password # Importing the password class
 
 class TestPassword(unittest.TestCase):
 
     '''
-    Test class that defines test cases for the contact class behaviours.
+    Test class that defines test cases for the password class behaviours.
 
     Args:
         unittest.TestCase: TestCase class that helps in creating test cases
     '''
+def setUp(self):
+        '''
+        Set up method to run before each test cases.
+        '''
+        self.new_password = Password("Faith", "123456") # create password object
+
+
+def test_init(self):
+    '''
+    test_init test case to test if the object is initialized properly
+    '''
+
+    self.assertEqual(self.new_password.user_name,"Faith")
+    self.assertEqual(self.new_password.password,"123456")
+    
+
+if __name__ == '__main__':
+    unittest.main()
