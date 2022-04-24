6c44f888c3f7ca02281ff48fd7ff979cbaf35880
faith62 <phifeichemu@gmail.com>
2022-04-24 20:19:27 +0300

    create empty password list variable to store password objects

--- password.py
+++ password.py
@@ -2,6 +2,8 @@ class Password:
     """
     Class that generates new instances of password
     """
+    password_list =  [] #used to store our created password objects 
+    
     def __init__(self,user_name,password):
         self.user_name = user_name
         self.password = password
