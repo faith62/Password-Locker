6fea1c9ab5232da912f7d6d5e2eb94f19fc987b7
faith62 <phifeichemu@gmail.com>
2022-04-24 20:23:52 +0300

    import Python test framework called unittest , create subclass that inherists from unittest.TestCase

---
+++ password_test.py
@@ -0,0 +1,11 @@
+import unittest # Importing the unittest module
+from password import Password # Importing the contact class
+
+class TestPassword(unittest.TestCase):
+
+    '''
+    Test class that defines test cases for the contact class behaviours.
+
+    Args:
+        unittest.TestCase: TestCase class that helps in creating test cases
+    '''
