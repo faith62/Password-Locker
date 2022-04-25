1ba15b78fca41959d5f725f2ddb805e6fe8894b0
faith62 <phifeichemu@gmail.com>
2022-04-25 17:51:39 +0300

    create function to create new user

--- run.py
+++ run.py
@@ -2,6 +2,13 @@
 
 from password import User, Credentials
 
+def create_user(username, password):
+    '''
+    Function to create new user
+    '''
+    new_user = User(username,password)
+    return new_user
+
 def create_credentials(account,username,password):
     '''
     Function to create a new credentials
