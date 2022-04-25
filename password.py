4e4abbbe0023e37e82cc68a287a53f7274e6b133
faith62 <phifeichemu@gmail.com>
2022-04-25 14:24:45 +0300

    change the class name from Password to Credentials as password is specific

--- password.py
+++ password.py
@@ -1,10 +1,10 @@
 import pyperclip
-from keyring import delete_password
+# from keyring import delete_credentials
 
 
 class User:
     """
-    Class that generates new instances of password
+    Class that generates new instances of user
     """
     user_list = [] #used to store our created user objects 
 
@@ -16,7 +16,7 @@ class User:
     def save_user(self):
 
         '''
-        save_user method saves password objects into user_list
+        save_user method saves user objects into user_list
         '''
 
         User.user_list.append(self)
@@ -24,7 +24,7 @@ class User:
     @classmethod
     def display_user(cls):
         '''
-        method that returns password list
+        method that returns user list
         '''
         return cls.user_list
 
@@ -35,53 +35,53 @@ class User:
         User.user_list.remove(self)
 
         
-class Password:
+class Credentials:
     """
     Class that generates new instances of password
     """
-    password_list =  [] #used to store our created password objects 
+    credentials_list =  [] #used to store our created credentials objects 
 
     def __init__(self,user_name,password):
         self.user_name = user_name
         self.password = password
 
-    def save_password(self):
+    def save_credentials(self):
 
         '''
-        save_password method saves password objects into password_list
+        save_credentials method saves credentials objects into credentials_list
         '''
 
-        Password.password_list.append(self)
+        Credentials.credentials_list.append(self)
         
 
-    def delete_password(self):
+    def delete_credentials(self):
         '''
-        delete_password method deletes a saved contact from the password_list
+        delete_credentials method deletes a saved contact from the credentials_list
         '''
 
-        Password.password_list.remove(self)
+        Credentials.credentials_list.remove(self)
 
     @classmethod
-    def find_by_password(cls,number):
-        for password in cls.password_list:
-            if password.password == password:
-                return password
+    def find_by_credentials(cls,number):
+        for credentials in cls.credentials_list:
+            if credentials.password == credentials:
+                return credentials
 
     @classmethod
-    def password_exist(cls,password):
-        for password in cls.password_list:
+    def credentials_exist(cls,password):
+        for credentials in cls.credentials_list:
             return True
 
         return False
 
     @classmethod
-    def display_password(cls):
+    def display_credentials(cls):
         '''
-        method that returns password list
+        method that returns credentials list
         '''
-        return cls.contact_list
+        return cls.credentials_list
 
     @classmethod
-    def copy_password(cls,password):
-        password_found = Password.find_by_password(password)
-        pyperclip.copy(password_found.password) #copy passed in items to the machines clipboard
+    def copy_credentials(cls,password):
+        credentials_found = Credentials.find_by_credentials(password)
+        pyperclip.copy(credentials_found.password) #copy passed in items to the machines clipboard
